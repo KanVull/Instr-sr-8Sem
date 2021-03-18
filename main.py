3e3cfbcdcf78f1fae7d2c79bc0375c2bcd198e27
KanVull <minirorus7@gmail.com>
2021-03-18 14:16:15 +0300

    post, delete

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import xml.etree.ElementTree as ET
 import random
 import json
 
+xml_file = ''
+
 def load_xml():
     tree = ET.parse('quotes.xml')
     return tree
 
-def get_quote_dict(id, xml_file):
+
+def get_quote_dict(id):
     quote = xml_file.find(f"quote[@id='{id}']")
     if quote is not None:
         d = {
@@ -20,26 +23,26 @@ def get_quote_dict(id, xml_file):
     else:    
         return None 
 
-def write_in_xml(quote, xml_file):
+
+def write_in_xml(quote):
     q = ET.Element('quote')
-    q.set('id', quote['id'])
+    q.set('id', str(quote['id']))
     author = ET.SubElement(q, 'author')
     author.text = quote['author']
     quote_text = ET.SubElement(q, 'quote_text')
     quote_text.text = quote['quote']
-    xml_file.append(q)
+    root = xml_file.getroot()
+    root.append(q)
+    xml_file.write('quotes.xml')
     return True
 
-
-app = Flask(__name__)
-api = Api(app)
 
 class Quote(Resource):
     def get(self, id=0):
         if id == 0:
             paragraphs = xml_file.findall('//quote')
-            return get_quote_dict(random.randint(1, len(paragraphs)), xml_file), 200
-        quote = get_quote_dict(id, xml_file)
+            return get_quote_dict(random.randint(1, len(paragraphs))), 200
+        quote = get_quote_dict(id)
         if quote is not None:    
             return quote, 200
         return f'Quote with id {id} not found', 404
@@ -51,7 +54,7 @@ class Quote(Resource):
         parser.add_argument('author')
         parser.add_argument('quote_text')
         params = parser.parse_args()
-        d = get_quote_dict(quote['id'], xml_file)
+        d = get_quote_dict(params['id'])
         if d is not None:
             return f'Quote with id {params["id"]} already exists', 400
         quote = {
@@ -60,36 +63,27 @@ class Quote(Resource):
             'quote': params['quote_text'],
         }
         write_in_xml(quote)
-        return quote, 201
+        return quote, 201    
 
 
-    def put(self, id):
+    def delete(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('id')
-        parser.add_argument('author')
-        parser.add_argument('quote')
+        parser.add_argument('id', type=int)
         params = parser.parse_args()
-        for quote in ai_quotes:
-            if(params['id'] == quote['id']):
-                quote['author'] = params['author']
-                quote['quote'] = params['quote']
-                return quote, 200
-        quote = {
-            'id': params['id'],
-            'author': params['author'],
-            'quote': params['quote'],
-        }
-        ai_quotes.append(quote)
-        return quote, 201        
+        d = get_quote_dict(params['id'])
+        if d is None:
+            return f'Quote with id {params["id"]} not exists', 400
+        root = xml_file.getroot()
+        quote = xml_file.find(f"quote[@id='{params['id']}']")
+        root.remove(quote)
+        xml_file.write('quotes.xml')
+        return f'Quote with id {params['id']} is deleted.', 200    
 
 
-    def delete(self, id):
-        global ai_quotes
-        ai_quotes = [quote for quote in ai_quotes if qouote["id"] != id]
-        return f'Quote with id {id} is deleted.', 200    
-
+app = Flask(__name__)
+api = Api(app)
 
 api.add_resource(Quote, "/ai-quotes", "/ai-quotes/", "/ai-quotes/<int:id>")
 if __name__ == '__main__':
     xml_file = load_xml()      
-    app.run(debug=True)
+    app.run(debug=False, ssl_context='adhoc')
